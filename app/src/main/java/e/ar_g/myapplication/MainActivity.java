15c35f11950754bab4231f0a66e37650c9737c88
Andrii Rakhimov <geleznodorognik0109@gmail.com>
2018-11-05 20:53:01 +0200

    Homework 1

--- app/src/main/java/e/ar_g/myapplication/MainActivity.java
+++ app/src/main/java/e/ar_g/myapplication/MainActivity.java
@@ -1,13 +1,33 @@
 package e.ar_g.myapplication;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+  private TextView label;
+  private int counter = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Log.e("MainActivity", "onCreate called");
+
+    label = findViewById(R.id.label);
+    label.setText("Андроид ноябрь");
+    counter++;
+  }
+
+  @Override protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putInt("counter", counter);
+  }
+
+  @Override protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    counter = savedInstanceState.getInt("counter");
   }
 }
